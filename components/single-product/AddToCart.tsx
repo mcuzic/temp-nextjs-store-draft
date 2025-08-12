1dc0b7fc018923b04d0f944e2e622f5ed3def97f
mcuzic <matija.matijas.cuzic@gmail.com>
2025-08-12 22:49:04 +0200

    Fix AddToCart misspeled ProductId

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,6 +1,6 @@
 import { Button } from '../ui/button';
 
-const AddToCart = ({ productsId }: { productId: string }) => {
+const AddToCart = ({ productId }: { productId: string }) => {
   return <Button className="capitalize mt-8 size='lg">Add to Cart</Button>;
 };
 export default AddToCart;
