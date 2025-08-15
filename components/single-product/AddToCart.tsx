18ddb61f899f371fa10c1d55fcf7c49b4cc7a2db
mcuzic <matija.matijas.cuzic@gmail.com>
2025-08-15 22:42:11 +0200

    test

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,6 +1,6 @@
 import { Button } from '../ui/button';
 
 const AddToCart = ({ productId }: { productId: string }) => {
-  return <Button className="capitalize mt-8 size='lg">Add to Carts</Button>;
+  return <Button className="capitalize mt-8 size='lg">Add to Cart</Button>;
 };
 export default AddToCart;
