6163c14fd7c388c5cf2c2685ba6d10ee35bca5f8
mcuzic <matija.matijas.cuzic@gmail.com>
2025-10-05 19:55:55 +0200

    test

--- utilis/supabase.ts
+++ utilis/supabase.ts
@@ -1,27 +1,25 @@
-// import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-// const bucket = 'mian-bucket';
+const bucket = 'mian-bucket';
 
-// export const supabase = createClient(
-//   process.env.SUPABASE_URL as string,
-//   process.env.SUPABASE_KEY as string
-// );
+export const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string
+);
 
 export const uploadImage = async (image: File) => {
-  // const timestamp = Date.now();
-  // const newName = `${timestamp}-${image.name}`;
-  // const { data } = await supabase.storage
-  //   .from(bucket)
-  //   .upload(newName, image, { cacheControl: '3600' });
+  const timestamp = Date.now();
+  const newName = `${timestamp}-${image.name}`;
+  const { data } = await supabase.storage
+    .from(bucket)
+    .upload(newName, image, { cacheControl: '3600' });
 
-  // if (!data) throw new Error('Image upload failed');
-  // return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
-  return '/placeholder.jpg'; // return a static URL
+  if (!data) throw new Error('Image upload failed');
+  return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
 
 export const deleteImage = (url: string) => {
-  // const imageName = url.split('/').pop();
-  // if (!imageName) throw new Error('Invalid URL');
-  // return supabase.storage.from(bucket).remove([imageName]);
-  return '/placeholder.jpg'; // return a static URL
+  const imageName = url.split('/').pop();
+  if (!imageName) throw new Error('Invalid URL');
+  return supabase.storage.from(bucket).remove([imageName]);
 };
