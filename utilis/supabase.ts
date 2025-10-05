30ce9326bdc0d892febc6f1f860d1dbcd73286fc
mcuzic <matija.matijas.cuzic@gmail.com>
2025-10-05 20:01:53 +0200

    test

--- utilis/supabase.ts
+++ utilis/supabase.ts
@@ -1,10 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const bucket = 'mian-bucket';
+const SUPABASE_URL = 'https://jbegmqmmkylhyxmzznvy.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZWdtcW1ta3lsaHl4bXp6bnZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzODgxNjgsImV4cCI6MjA2ODk2NDE2OH0.Lmqis1D3zYFYOrqSLDkdEmJ7UXcgSPUgPYpg3zQ6Glk';
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  SUPABASE_URL as string,
+  SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
