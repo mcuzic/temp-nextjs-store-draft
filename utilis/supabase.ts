480721e6a4dd70ee686418f029c278b9c3c0dd29
mcuzic <matija.matijas.cuzic@gmail.com>
2025-10-05 20:10:29 +0200

    added 'use server

--- utilis/supabase.ts
+++ utilis/supabase.ts
@@ -1,13 +1,12 @@
+'us server';
+
 import { createClient } from '@supabase/supabase-js';
 
 const bucket = 'mian-bucket';
-const SUPABASE_URL = 'https://jbegmqmmkylhyxmzznvy.supabase.co';
-const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiZWdtcW1ta3lsaHl4bXp6bnZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzODgxNjgsImV4cCI6MjA2ODk2NDE2OH0.Lmqis1D3zYFYOrqSLDkdEmJ7UXcgSPUgPYpg3zQ6Glk';
 
 export const supabase = createClient(
-  SUPABASE_URL as string,
-  SUPABASE_KEY as string
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
@@ -16,7 +15,6 @@ export const uploadImage = async (image: File) => {
   const { data } = await supabase.storage
     .from(bucket)
     .upload(newName, image, { cacheControl: '3600' });
-
   if (!data) throw new Error('Image upload failed');
   return supabase.storage.from(bucket).getPublicUrl(newName).data.publicUrl;
 };
