db474850f31c78a91ea82eee11ef073e54d4de57
mcuzic <matija.matijas.cuzic@gmail.com>
2025-10-05 21:16:34 +0200

    Vite_prefix

--- utilis/supabase.ts
+++ utilis/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const bucket = 'mian-bucket';
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.VITE_SUPABASE_URL as string,
+  process.env.VITE_SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
